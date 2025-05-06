18a57d6044193657c1f5484109d9d315e527e3cb
Allen <valternatean13@gmail.com>
2025-05-06 11:30:27 +0700

    ref(nvim): replace `mini.statusline` to `lualine`

---
+++ nvim/lua/custom/plugins/lualine.lua
@@ -0,0 +1,49 @@
+-- Configurable statusline
+
+return {
+  'nvim-lualine/lualine.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+
+  -- Props for the correction: https://gist.github.com/roymanigley/0987234b8cf5f7ea53b191abe17094c6
+  opts = {
+    options = {
+      icons_enabled = true,
+      theme = 'auto',
+      component_separators = { left = '', right = '' },
+      section_separators = { left = '▒', right = '▒' },
+      disabled_filetypes = {
+        statusline = {},
+        winbar = {},
+      },
+      ignore_focus = {},
+      always_divide_middle = true,
+      always_show_tabline = true,
+      globalstatus = false,
+      refresh = {
+        statusline = 100,
+        tabline = 100,
+        winbar = 100,
+      },
+    },
+    sections = {
+      lualine_a = { 'mode' },
+      lualine_b = { 'branch', 'diff', 'diagnostics' },
+      lualine_c = { 'filename' },
+      lualine_x = {},
+      lualine_y = { 'encoding', 'fileformat', 'filetype' },
+      lualine_z = { 'location' },
+    },
+    inactive_sections = {
+      lualine_a = {},
+      lualine_b = {},
+      lualine_c = { 'filename' },
+      lualine_x = { 'location' },
+      lualine_y = {},
+      lualine_z = {},
+    },
+    tabline = {},
+    winbar = {},
+    inactive_winbar = {},
+    extensions = {},
+  },
+}
